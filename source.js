976a0707fe69f4f3d8223cedacc439e565cd5330
milesvkean <mileskean@hotmail.com>
2021-12-31 13:49:12 -0600

    Add source

---
+++ source.js
@@ -0,0 +1,15 @@
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        cell.addEventListener("mouseover", function () {
+            cell.style.backgroundColor = "green";
+        });
+        container.appendChild(cell).className = "grid-item";
+    };
+};
+
+makeRows(16, 16);
